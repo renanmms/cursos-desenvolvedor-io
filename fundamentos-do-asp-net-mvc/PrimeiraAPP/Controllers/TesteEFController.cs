ce1462116ab46ae57a3112ad870d6ced03c0c84f
Renan Martins <renanmartins1999@hotmail.com>
2024-08-31 23:07:08 -0300

    feat: adicionando controller para testes do EF

---
+++ fundamentos-do-asp-net-mvc/PrimeiraAPP/Controllers/TesteEFController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraAPP.Data;
+using PrimeiraAPP.Models;
+
+namespace PrimeiraAPP.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        private readonly AppDbContext Db;
+        public TesteEFController(AppDbContext db)
+        {
+            Db = db;    
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Renan",
+                Email = "hello@renan.com",
+                DataNascimento = new DateTime(1999, 12, 4),
+                Ativo = true,
+                Avaliacao = 5
+            };
+
+            Db.Alunos.Add(aluno);
+            Db.SaveChanges();
+
+            return Content("");
+        }
+    }
+}
