e85210ca6b6ba3d32406ca37fdd084fab8de5303
Renan Martins <renanmartins1999@hotmail.com>
2024-08-31 22:54:45 -0300

    feat: adicionando data annotations

--- fundamentos-do-asp-net-mvc/PrimeiraAPP/Models/Aluno.cs
+++ fundamentos-do-asp-net-mvc/PrimeiraAPP/Models/Aluno.cs
@@ -11,11 +11,25 @@ namespace PrimeiraAPP.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto")]
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Formato inválido de e-mail")]
         public string? Email { get; set; }
 
         [NotMapped]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Formato inválido de e-mail")]
         public string? EmailConfirmacao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, 5, ErrorMessage = "O campo {0} deve ester entre {1} e {2}")]
         public int Avaliacao { get; set; }
         public bool Ativo { get; set; }
     }
