3e5633d0f4a1a93394968b015acbd428bd27331e
Renan Martins <renanmartins1999@hotmail.com>
2024-10-28 01:25:25 -0300

    feat: adicionar scaffold para aluno

--- fundamentos-do-asp-net-mvc/MinhaAppFuncional/src/AppMvcFuncional/Models/ErrorViewModel.cs
+++ fundamentos-do-asp-net-mvc/MinhaAppFuncional/src/AppMvcFuncional/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace AppMvcFuncional.Models;
 
 public class ErrorViewModel
 {
+    // TODO: Criar um modelo para Aluno
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
